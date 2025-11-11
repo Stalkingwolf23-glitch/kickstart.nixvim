a4a7a7ba6face8cf254d5715333727b02412e72d
Stalkingwolf <stakingwolf23@gmail.com>
2025-11-11 19:02:58 +0800

    Added: highlight-colors

---
+++ config/plugins/custom/highlight-colors.nix
@@ -0,0 +1,18 @@
+{
+  plugins.highlight-colors = {
+    enable = true;
+    settings = {
+      render = "virtual";
+      virtual_symbol = "■";
+      enable_hex = true;
+      enable_short_hex = true;
+      enable_hsl = true;
+      enable_rgb = true;
+      enable_ansi = true;
+      enable_hsl_without_function = true;
+      enable_named_colors = true;
+      enable_var_usage = true;
+      enable_tailwind = false;
+    };
+  };
+}
